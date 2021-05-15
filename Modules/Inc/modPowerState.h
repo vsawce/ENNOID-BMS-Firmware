b9c4a499900cffccbd88c350edc8efefd23fc7fa
Vincent <vsaw30@gmail.com>
2021-05-15 15:03:07 -0700

    Fixed include directive, case sensitive

--- Modules/Inc/modPowerState.h
+++ Modules/Inc/modPowerState.h
@@ -20,7 +20,7 @@
  
 #include "modDelay.h"
 #include "driverHWPowerState.h"
-#include "modconfig.h"
+#include "modConfig.h"
 
 #define POWERBUTTON_POWERDOWN_THRESHOLD_TIME 1000
 #define POWERBUTTON_FORCEON_THRESHOLD_TIME 5000
