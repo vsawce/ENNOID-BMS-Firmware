5da77b263abefc5a533d4ad4b8946c1eecf926be
Sean Lin <47756378+seanlinc@users.noreply.github.com>
2021-06-13 12:31:21 -0700

    initialize state of charge files (#12)

---
+++ Modules/Inc/state_of_charge.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "modPowerElectronics.h"
+#include "modConfig.h"
+
+
+
---
+++ Modules/Src/state_of_charge.c
@@ -0,0 +1,7 @@
+#include "state_of_charge.h"
+
+typedef struct {
+	float state_of_charge; // accuraty 0.1 
+
+
+}
