4e0f5b8c88d69d6592cb77bb00a48f3c534ce0e4
Vincent Saw <56854473+vsawce@users.noreply.github.com>
2021-06-21 13:01:00 -0700

    Swapped temp board order, moved board 2 to end of array (#18)

--- Drivers/SWDrivers/Src/driverSWADC128D818.c
+++ Drivers/SWDrivers/Src/driverSWADC128D818.c
@@ -25,7 +25,12 @@ uint8_t driverSWADC128D818TotalNoOfExpansionBoard = 0;
 uint8_t driverSWADC128D818MaxNoOfTempSensorPerExpansionBoard = 0;
 
 uint32_t driverSWADC128StartupDelayLastTick;
-static const uint8_t ADC128Address[9] ={0x1D,0x1E,0x1F,0x2D,0x2E,0x2F,0x35,0x36,0x37};
+
+//Regular Sequence (1 2 3 4 5 6 7 8)
+//static const uint8_t ADC128Address[9] ={0x1D,0x1E,0x1F,0x2D,0x2E,0x2F,0x35,0x36,0x37};
+//Current Sequence (1 3 4 5 6 7 8 2)
+static const uint8_t ADC128Address[9] ={0x1D,0x1F,0x2D,0x2E,0x2F,0x35,0x36,0x37,0x1E};
+
 
 void driverSWADC128D818Init(uint8_t noOfExpansionBoard, uint8_t noOfTempSensorPerExpansionBoard){
 	
